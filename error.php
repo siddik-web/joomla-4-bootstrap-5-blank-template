a51f8a3c679d7ec231b6f2c301024c9a734018cc
Md Siddiqur Rahman <siddikcoder@gmail.com>
2022-05-22 12:24:04 +0600

    add module positions

--- error.php
+++ error.php
@@ -33,6 +33,7 @@ defined('_JEXEC') or die;
     <jdoc:include type="modules" name="right" style="none" />
     <jdoc:include type="modules" name="footer" style="none" />
     <jdoc:include type="modules" name="debug" style="none" />
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 
